ca4f8933c20c4dcbc03938c63747fbd56ab0fe90
BatuhanAC <70262961+BatuhanAC@users.noreply.github.com>
2023-01-09 14:37:21 +0300

    1

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const corsOptions = require("./src/helpers/corsOptions")
 app.use(express.json())
 app.use(express.json({limit: "50mb"}))
 app.use(express.urlencoded({limit: "50mb", extended: true, parameterLimit: 50000}))
-app.use(cors(corsOptions))
+
 app.use("/api", router)
 app.use(errorHandlerMiddleware)
 
