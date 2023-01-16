b4198619576956fb32d26639b538e2f35835ff9a
BatuhanAC <70262961+BatuhanAC@users.noreply.github.com>
2023-01-16 17:51:19 +0300

    Update errorHandler.js

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -4,12 +4,12 @@ const errorHandlerMiddleware = (err, req, res, next) => {
   if (err instanceof APIError) {
     return res.status(err.statusCode || 400)
       .json({
-        succes: false,
+        success: false,
         message: err.message
       })
   }
   return res.status(500).json({
-    succes: false,
+    success: false,
     message: "An error has ocurred! Please check your API."
   })
 }
