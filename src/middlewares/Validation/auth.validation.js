426ae36c61daf44e1f3081a5368eccd11680f8c4
BatuhanAC <70262961+BatuhanAC@users.noreply.github.com>
2023-01-22 18:01:00 +0300

    progress validasyon joi

--- src/middlewares/Validation/auth.validation.js
+++ src/middlewares/Validation/auth.validation.js
@@ -47,7 +47,6 @@ class AuthValidation {
   }
 
   static login = async (req, res, next) => {
-    console.log(req.body)
     try {
       await joi.object({
         email: joi.string().email().trim().min(3).max(100).required().messages({
@@ -78,23 +77,59 @@ class AuthValidation {
 
 
   static progress = async (req, res, next) => {
+    console.log(req.body)
     try {
       await joi.object({
-        values: joi.string().email().trim().min(3).max(100).required().messages({
-          "string.base": "Email name has to be normal text.",
-          "string.email": "Email is unvalid.",
-          "string.empty": "Email can't be empty.",
-          "string.min": "Email must be more than 3 chars.",
-          "string.max": "Email must be less than 100 chars.",
-          "string.required": "Email required."
-        }),
-        password: joi.string().trim().min(6).max(36).required().messages({
-          "string.base": "Password has to be normal text.",
-          "string.empty": "Password can't be empty.",
-          "string.min": "Password must be more than 6 chars.",
-          "string.max": "Password must be less than 36 chars.",
-          "string.required": "Password required."
-        }),
+        values: joi.array({
+          neck: joi.number().min(0).max(999).required().messages({
+            "number.base": "Neck value has to be a number.",
+            "number.min": "Neck value can't be less than 0.",
+            "number.max": "Neck value has to be less than 1000.",
+            "number.required": "Neck value required."
+          }),
+          chest: joi.number().min(0).max(999).required().messages({
+            "number.base": "Chest value has to be a number.",
+            "number.min": "Chest value can't be less than 0.",
+            "number.max": "Chest value has to be less than 1000.",
+            "number.required": "Chest value required."
+          }),
+          waist: joi.number().min(0).max(999).required().messages({
+            "number.base": "Waist value has to be a number.",
+            "number.min": "Waist value can't be less than 0.",
+            "number.max": "Waist value has to be less than 1000.",
+            "number.required": "Waist value required."
+          }),
+          hip: joi.number().min(0).max(999).required().messages({
+            "number.base": "Hip value has to be a number.",
+            "number.min": "Hip value can't be less than 0.",
+            "number.max": "Hip value has to be less than 1000.",
+            "number.required": "Hip value required."
+          }),
+          arm: joi.number().min(0).max(999).required().messages({
+            "number.base": "Arm value has to be a number.",
+            "number.min": "Arm value can't be less than 0.",
+            "number.max": "Arm value has to be less than 1000.",
+            "number.required": "Arm value required."
+          }),
+          weight: joi.number().min(0).max(999).required().messages({
+            "number.base": "Weight value has to be a number.",
+            "number.min": "Weight value can't be less than 0.",
+            "number.max": "Weight value has to be less than 1000.",
+            "number.required": "Weight value required."
+          }),
+          fat: joi.number().min(0).max(999).required().messages({
+            "number.base": "Fat value has to be a number.",
+            "number.min": "Fat value can't be less than 0.",
+            "number.max": "Fat value has to be less than 1000.",
+            "number.required": "Fat value required."
+          }),
+          date: joi.number().required().messages({
+            "number.required": "Couldn't reach date value."
+          }),
+          day: joi.number().required().messages({
+            "number.required": "Couldn't reach day value."
+          })
+        })
       }).validateAsync(req.body)
     } catch (error) {
       try {
